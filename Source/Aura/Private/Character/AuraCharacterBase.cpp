7ed4e784e4104558cdc1ecfc7a31111825dd6998
Andrey Dyuzhikov <146117567+Anhair32167@users.noreply.github.com>
2024-08-23 13:37:54 +0300

    Player and Enemy Character

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,21 @@
+// AnhairEntertainment
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{ 
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// AnhairEntertainment
+
+
+#include "Character/AuraEnemy.h"
+
